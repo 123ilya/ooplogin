226304dfeacd5481e41a13155b689a8842105e6e
123ilya <123ilya@gmail.com>
2023-07-01 15:07:13 +1000

    the sketch of register form is ready

---
+++ register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+
+<body>
+    <form action="" method="post">
+        <div class="field">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" value="" autocomplete="off">
+        </div>
+        <div class="field">
+            <label for="password">Choose a password</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div class="field">
+            <label for="password_again">Enter your password again</label>
+            <input type="password" name="password_again" id="password_again">
+        </div>
+        <div class="field">
+            <label for="name">Lorem ipsum </label>
+            <input type="text" name="name" id="name">
+        </div>
+        <input type="submit" value="Register">
+    </form>
+</body>
+
+</html>
