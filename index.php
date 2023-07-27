bb112a2886e57f38236b85552419c7e393cfccea
123ilya <123ilya@gmail.com>
2023-07-27 18:57:58 +1000

    add sql file

--- index.php
+++ index.php
@@ -6,3 +6,4 @@ require_once './core/init.php';
 if (Session::exists('home')) {
     echo '<p>' . Session::flash('home') . '</p>';
 }
+echo '<p>'.Session::get(Config::get('session/session_name')).'</p>';
